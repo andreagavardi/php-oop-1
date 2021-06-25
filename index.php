db37fc8d4e0b3eb0760028aac0a35c29b6450e33
andreagavardi <andreagavardi89@gmail.com>
2021-06-25 14:49:17 +0200

    creata lista di Movies

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ class Movie
     public $genre;
     public $desc;
     public $poster;
+    public $vote;
 
     function __construct($title, $genre, $desc, $poster = 'https://www.thismarketerslife.it/site/wp-content/uploads/2016/05/pellicola1.jpg')
     {
@@ -15,8 +16,28 @@ class Movie
         $this->desc = $desc;
         $this->poster = $poster;
     }
+
+    public function setVote($vote)
+    {
+        $this->vote = $vote;
+    }
 }
 
-$starWars = new Movie('Star-Wars', 'fantasy', 'bla bla bla bla');
+//$starWars = new Movie('Star-Wars', 'fantasy', 'bla bla bla bla');
+//$starWars->setVote(10);
+//var_dump($starWars);
 
-var_dump($starWars);
+$Movies = [
+    $starWars = new Movie('Star-Wars', 'fantasy', 'bla bla bla bla'),
+    $Constantine = new Movie('Constantine', 'fantasy', 'bla bla bla bla'),
+    $reLeone = new Movie('Lion-King', 'animation', 'bla bla bla bla'),
+    $Interstellar = new Movie('Interstellar', 'sci-fi', 'bla bla bla bla'),
+    $lordOfRings = new Movie('Lord Of Rings', 'fantasy', 'bla bla bla bla'),
+    $TheTrumanShow = new Movie('The Truman Show', 'Drama', 'bla bla bla bla'),
+    $Greese = new Movie('Greese', 'musical', 'bla bla bla bla'),
+    $TheRing = new Movie('The Ring', 'fantasy', 'bla bla bla bla'),
+
+
+];
+
+echo '<pre>', var_dump($Movies), '</pre>';
